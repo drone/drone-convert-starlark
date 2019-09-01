6efeee3be3ef7fb42c7095a32f11154439447a62
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-09-01 16:49:02 -0700

    fix build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,6 +10,6 @@ set -e
 set -x
 
 # linux
-GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/drone-runner-ssh
-GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/drone-runner-ssh
-GOOS=linux GOARCH=arm   go build -o release/linux/arm/drone-runner-ssh
+GOOS=linux GOARCH=amd64 go build -o release/linux/amd64/drone-convert-starlark
+GOOS=linux GOARCH=arm64 go build -o release/linux/arm64/drone-convert-starlark
+GOOS=linux GOARCH=arm   go build -o release/linux/arm/drone-convert-starlark
