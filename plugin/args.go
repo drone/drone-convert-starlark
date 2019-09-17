eba4f73f854dbdde8d78d20f07203a6fc9c7b202
Brad Rydzewski <brad.rydzewski@gmail.com>
2019-09-17 11:13:54 -0700

    use starlark structs instead of maps

--- plugin/args.go
+++ plugin/args.go
@@ -6,7 +6,9 @@ package plugin
 
 import (
 	"github.com/drone/drone-go/drone"
+
 	"go.starlark.net/starlark"
+	"go.starlark.net/starlarkstruct"
 )
 
 // TODO(bradrydzewski) add repository id
@@ -21,7 +23,6 @@ import (
 // TODO(bradrydzewski) add build number
 // TODO(bradrydzewski) add build parent
 // TODO(bradrydzewski) add build timestamp
-// TODO(bradrydzewski) add build params
 // TODO(bradrydzewski) add build started
 // TODO(bradrydzewski) add build finished
 // TODO(bradrydzewski) add build created
@@ -30,54 +31,71 @@ import (
 // TODO(bradrydzewski) add build created
 
 func createArgs(repo drone.Repo, build drone.Build) []starlark.Value {
-	dict := new(starlark.Dict)
-	dict.SetKey(starlark.String("build"), fromBuild(build))
-	dict.SetKey(starlark.String("repo"), fromRepo(repo))
-	return starlark.Tuple([]starlark.Value{dict})
+	return []starlark.Value{
+		starlarkstruct.FromStringDict(
+			starlark.String("context"),
+			starlark.StringDict{
+				"repo":  starlarkstruct.FromStringDict(starlark.String("repo"), fromRepo(repo)),
+				"build": starlarkstruct.FromStringDict(starlark.String("build"), fromBuild(build)),
+			},
+		),
+	}
 }
 
-func fromBuild(v drone.Build) *starlark.Dict {
-	dict := new(starlark.Dict)
-	dict.SetKey(starlark.String("event"), starlark.String(v.Event))
-	dict.SetKey(starlark.String("action"), starlark.String(v.Action))
-	dict.SetKey(starlark.String("cron"), starlark.String(v.Cron))
-	dict.SetKey(starlark.String("environent"), starlark.String(v.Deploy))
-	dict.SetKey(starlark.String("link"), starlark.String(v.Link))
-	dict.SetKey(starlark.String("branch"), starlark.String(v.Target))
-	dict.SetKey(starlark.String("source"), starlark.String(v.Source))
-	dict.SetKey(starlark.String("before"), starlark.String(v.Before))
-	dict.SetKey(starlark.String("after"), starlark.String(v.After))
-	dict.SetKey(starlark.String("target"), starlark.String(v.Target))
-	dict.SetKey(starlark.String("ref"), starlark.String(v.Ref))
-	dict.SetKey(starlark.String("commit"), starlark.String(v.After))
-	dict.SetKey(starlark.String("title"), starlark.String(v.Title))
-	dict.SetKey(starlark.String("message"), starlark.String(v.Message))
-	dict.SetKey(starlark.String("source_repo"), starlark.String(v.Fork))
-	dict.SetKey(starlark.String("author_login"), starlark.String(v.Author))
-	dict.SetKey(starlark.String("author_name"), starlark.String(v.AuthorName))
-	dict.SetKey(starlark.String("author_email"), starlark.String(v.AuthorEmail))
-	dict.SetKey(starlark.String("author_avatar"), starlark.String(v.AuthorAvatar))
-	dict.SetKey(starlark.String("sender"), starlark.String(v.Sender))
-	return dict
+func fromBuild(v drone.Build) starlark.StringDict {
+	return starlark.StringDict{
+		"event":         starlark.String(v.Event),
+		"action":        starlark.String(v.Action),
+		"cron":          starlark.String(v.Cron),
+		"environent":    starlark.String(v.Deploy),
+		"link":          starlark.String(v.Link),
+		"branch":        starlark.String(v.Target),
+		"source":        starlark.String(v.Source),
+		"before":        starlark.String(v.Before),
+		"after":         starlark.String(v.After),
+		"target":        starlark.String(v.Target),
+		"ref":           starlark.String(v.Ref),
+		"commit":        starlark.String(v.After),
+		"title":         starlark.String(v.Title),
+		"message":       starlark.String(v.Message),
+		"source_repo":   starlark.String(v.Fork),
+		"author_login":  starlark.String(v.Author),
+		"author_name":   starlark.String(v.AuthorName),
+		"author_email":  starlark.String(v.AuthorEmail),
+		"author_avatar": starlark.String(v.AuthorAvatar),
+		"sender":        starlark.String(v.Sender),
+		"params":        fromMap(v.Params),
+	}
 }
 
-func fromRepo(v drone.Repo) *starlark.Dict {
+func fromRepo(v drone.Repo) starlark.StringDict {
+	return starlark.StringDict{
+		"uid":                  starlark.String(v.UID),
+		"name":                 starlark.String(v.Name),
+		"namespace":            starlark.String(v.Namespace),
+		"slug":                 starlark.String(v.Slug),
+		"git_http_url":         starlark.String(v.HTTPURL),
+		"git_ssh_url":          starlark.String(v.SSHURL),
+		"link":                 starlark.String(v.Link),
+		"branch":               starlark.String(v.Branch),
+		"config":               starlark.String(v.Config),
+		"private":              starlark.Bool(v.Private),
+		"visibility":           starlark.String(v.Visibility),
+		"active":               starlark.Bool(v.Active),
+		"trusted":              starlark.Bool(v.Trusted),
+		"protected":            starlark.Bool(v.Protected),
+		"ignore_forks":         starlark.Bool(v.IgnoreForks),
+		"ignore_pull_requests": starlark.Bool(v.IgnorePulls),
+	}
+}
+
+func fromMap(m map[string]string) *starlark.Dict {
 	dict := new(starlark.Dict)
-	dict.SetKey(starlark.String("uid"), starlark.String(v.UID))
-	dict.SetKey(starlark.String("name"), starlark.String(v.Name))
-	dict.SetKey(starlark.String("namespace"), starlark.String(v.Namespace))
-	dict.SetKey(starlark.String("slug"), starlark.String(v.Slug))
-	dict.SetKey(starlark.String("git_http_url"), starlark.String(v.HTTPURL))
-	dict.SetKey(starlark.String("git_ssh_url"), starlark.String(v.SSHURL))
-	dict.SetKey(starlark.String("link"), starlark.String(v.Link))
-	dict.SetKey(starlark.String("branch"), starlark.String(v.Branch))
-	dict.SetKey(starlark.String("config"), starlark.String(v.Config))
-	dict.SetKey(starlark.String("private"), starlark.Bool(v.Private))
-	dict.SetKey(starlark.String("visibility"), starlark.String(v.Visibility))
-	dict.SetKey(starlark.String("active"), starlark.Bool(v.Active))
-	dict.SetKey(starlark.String("trusted"), starlark.Bool(v.Trusted))
-	dict.SetKey(starlark.String("protected"), starlark.Bool(v.Protected))
-	dict.SetKey(starlark.String("ignore_forks"), starlark.Bool(v.IgnoreForks))
-	dict.SetKey(starlark.String("ignore_pull_requests"), starlark.Bool(v.IgnorePulls))
+	for k, v := range m {
+		dict.SetKey(
+			starlark.String(k),
+			starlark.String(v),
+		)
+	}
 	return dict
 }
--- plugin/testdata/single.star
+++ plugin/testdata/single.star
@@ -1,5 +1,10 @@
 
 def main(ctx):
+  print(ctx.build)
+  print(ctx.build.commit)
+  print(ctx.repo)
+  print(ctx.repo.namespace)
+  print(ctx.repo.name)
   return {
     'kind': 'pipeline',
     'type': 'docker',
